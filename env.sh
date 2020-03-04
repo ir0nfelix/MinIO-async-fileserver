71636bffd11a90e242c34fa900eee04855ef5c63
Alex Vasilev <a.vasilyev@rm-tech.ru>
2020-03-04 17:18:41 +0300

    env.sh added

---
+++ env.sh
@@ -0,0 +1,19 @@
+export DEBUG=True
+
+export HOST=127.0.0.1
+export PORT=5000
+
+export MINIO_SCHEME=http
+export MINIO_HOST=127.0.0.1
+export MINIO_PORT=9000
+
+ export MINIO_ACCESS_KEY=hoK9VPtVI7gK
+ export MINIO_SECRET_KEY=ogkGpxr7ULtq
+
+export MINIO_BUCKET_NAME=sapsanshop
+export STORAGE_CLASS=storage.MinioStorage
+export MAX_FILE_SIZE=5242880
+export FILE_FIELD_NAME=file
+
+export BISTRO_IMAGE_WIDTH=110
+export BISTRO_IMAGE_HEIGHT=110
